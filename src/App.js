d651e5e46ebb17cc65bed5cdd904f0a538853d67
brome40 <85357500+brome40@users.noreply.github.com>
2023-01-14 23:15:30 -0500

    Add Typewriter and logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import brlogo from './brlogo.png';
+import TypewriterComponent from 'typewriter-effect';
 
 function App() {
   return (
     <div className="App">
       <header className="nav-bar">
+        <img src={brlogo} alt='ben rome logo'/>
       </header>
       <div className='main'>
         <div className='name'>Ben Rome</div>
+        <div className='title'>
+          <TypewriterComponent
+            options ={{
+              strings: ['Web Developer', 'Software Developer', 'UI/UX Developer', 'Frontend Developer'],
+              autoStart: true,
+              loop: true
+            }}
+          />
+        </div>
       </div>
     </div>
   );
