147b6818c16185da65b335a15a3e8ba0e10f4d5f
brome40 <85357500+brome40@users.noreply.github.com>
2023-03-21 08:29:28 -0400

    Complete home section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
-import TypewriterComponent from 'typewriter-effect';
-import { NavBar } from './components/NavBar.jsx';
+import React, { useState } from 'react'
+import './App.css'
+import TypewriterComponent from 'typewriter-effect'
+import { NavBar } from './components/NavBar.jsx'
 
 function App() {
   const [activeLink, setActiveLink] = useState('/');
 
-  const handleLinkClick = (link, event) => {
-    event.preventDefault();
+  const handleLinkClick = (link) => {
     setActiveLink(link);
     window.location.href = link;
   };
@@ -16,19 +15,22 @@ function App() {
     <div className="App">
       <NavBar onLinkClick={handleLinkClick}/>
       <div className='main'>
-        <div className='name'>Ben Rome</div>
-        {activeLink === '/' && <p>Home</p> }
-        {activeLink === '/about' && <p>About</p> }
-        {}
-        <div className='title'>
-          <TypewriterComponent
-            options ={{
-              strings: ['Web Developer', 'Software Developer', 'UI/UX Developer', 'Frontend Developer'],
-              autoStart: true,
-              loop: true
-            }}
-          />
+        <div className="home" title='home' style={{ border: '1px solid green' }}>
+          <div className='name'>Ben Rome</div>
+          <div className='title'>
+            <TypewriterComponent
+              options ={{
+                strings: ['Web Developer', 'Software Developer', 'UI/UX Developer', 'Front-end Developer', 'React Developer'],
+                autoStart: true,
+                loop: true
+              }}
+            />
+          </div>
+          <div className='intro'>
+            <p>Hi, I'm Ben. A passionate Front-end Developer based in Louisville, KY. Thanks for checking out my page!</p>
+          </div>
         </div>
+        <div className='about' title='about' style={{ border: '1px solid blue' }}></div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../css/NavBar.module.css';
+import React, { useState, useEffect } from 'react'
+import { Link, animateScroll as scroll } from "react-scroll"
+import styles from '../css/NavBar.module.css'
+import logo from '../brlogo.png'
 
 export const NavBar = ({ activeLink, onLinkClick }) => {
 
-  const handleLinkClick = (link, event) => {
-    onLinkClick(link, event);
+  const handleLinkClick = (link) => {
+    onLinkClick(link);
   }
 
   return (
-    <header className={styles.navBar}>
-      <button
-        className={styles.navButton}
-        onClick={(event) => handleLinkClick('/', event)}
-      >
-        Home
-      </button>
-      <button
-        className={styles.navButton}
-        onClick={(event) => handleLinkClick('/about', event)}
-      >
-        About Me
-      </button>
-      <button
-        className={styles.navButton}
-        onClick={(event) => handleLinkClick('/contact', event)}
-      >
-        Contact
-      </button>
-    </header>
+    <>
+      <header className={styles.navBar}>
+        <img src={logo} alt='ben rome logo'/>
+        <div className='links'>
+          <Link
+            to='home'
+            smooth
+            className={styles.navButton}
+          >
+            Home
+          </Link>
+          <Link
+            to='about'
+            smooth
+            className={styles.navButton}
+          >
+            About Me
+          </Link>
+          <Link
+            className={styles.navButton}
+          >
+            Contact
+          </Link>
+        </div>
+      </header>
+    </>
   );
 }
